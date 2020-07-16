53dd50cc50b2d04f364bdf3fdc3b75ce1aaa684c
Artem <a.kotov@nord-soft.com>
2020-07-16 12:38:33 +0300

    2

---
+++ precache-manifest.0715ef5850cf900a8fe52eca892428ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b68556da4a086da77624c4c32df733fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aacba36903983912a3f9",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d05b5ff99b577d967268",
+    "url": "/static/js/2.1afffaf8.chunk.js"
+  },
+  {
+    "revision": "881b4cd60baf4ebc476fd3c43b040b40",
+    "url": "/static/js/2.1afffaf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aacba36903983912a3f9",
+    "url": "/static/js/main.bc3f9a48.chunk.js"
+  },
+  {
+    "revision": "880ead2686ec4d3085c1",
+    "url": "/static/js/runtime-main.b40e28e2.js"
+  }
+]);
